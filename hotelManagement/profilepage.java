ad25b8df00d5372f0fd7bf408974511d3dbf6299
arvind-26 <ak5943436@gmail.com>
2024-01-25 13:00:35 +0530

    update profile

--- hotelManagement/profilepage.java
+++ hotelManagement/profilepage.java
@@ -42,96 +42,55 @@ public class profilepage extends JFrame implements ActionListener{
         head.setBounds(10, 10, 100, 40);
         add(head);
 
-        JLabel firstname = new JLabel("First Name :");
-        firstname.setFont(new Font("Raleway", Font.BOLD, 12));
-        firstname.setBounds(20, 70, 100, 56);
+        JLabel firstname = new JLabel("First Name :  "+ w[0]);
+        firstname.setFont(new Font("Raleway", Font.BOLD, 15));
+        firstname.setBounds(20, 70, 230, 56);
         add(firstname);
 
-        JLabel firstname1 = new JLabel(w[0]);
-        firstname1.setFont(new Font("Raleway", Font.BOLD, 12));
-        firstname1.setBounds(90, 70, 100, 56);
-        add(firstname1);
-
-        JLabel midname = new JLabel("Middle Name : ");
-        midname.setFont(new Font("Raleway", Font.BOLD, 12));
-        midname.setBounds(200, 70, 100, 56);
+        JLabel midname = new JLabel("Middle Name :  "+w[1]);
+        midname.setFont(new Font("Raleway", Font.BOLD, 15));
+        midname.setBounds(200, 70, 200, 56);
         add(midname);
-        JLabel midname1 = new JLabel(w[1]);
-        midname1.setFont(new Font("Raleway", Font.BOLD, 12));
-        midname1.setBounds(283, 70, 100, 56);
-        add(midname1);
 
-        JLabel lastname = new JLabel("Last Name : ");
-        lastname.setFont(new Font("Raleway", Font.BOLD, 12));
-        lastname.setBounds(395, 70, 100, 56);
+        JLabel lastname = new JLabel("Last Name :  "+w[2]);
+        lastname.setFont(new Font("Raleway", Font.BOLD, 15));
+        lastname.setBounds(395, 70, 230, 56);
         add(lastname);
-        JLabel lastname1 = new JLabel(w[2]);
-        lastname1.setFont(new Font("Raleway", Font.BOLD, 12));
-        lastname1.setBounds(465, 70, 100, 56);
-        add(lastname1);
 
-        JLabel uage = new JLabel("Age : ");
-        uage.setFont(new Font("Raleway", Font.BOLD, 12));
+        JLabel uage = new JLabel("Age :  "+w[3]);
+        uage.setFont(new Font("Raleway", Font.BOLD, 15));
         uage.setBounds(20, 120, 100, 56);
         add(uage);
-        JLabel uage1 = new JLabel(w[3]);
-        uage1.setFont(new Font("Raleway", Font.BOLD, 12));
-        uage1.setBounds(90, 120, 100, 56);
-        add(uage1);
 
-        JLabel Gender = new JLabel("Gender : ");
-        Gender.setFont(new Font("Raleway", Font.BOLD, 12));
-        Gender.setBounds(200, 120, 100, 56);
+        JLabel Gender = new JLabel("Gender :  "+w[4]);
+        Gender.setFont(new Font("Raleway", Font.BOLD, 15));
+        Gender.setBounds(200, 120, 200, 56);
         add(Gender);
-        JLabel Gender1 = new JLabel(w[4]);
-        Gender1.setFont(new Font("Raleway", Font.BOLD, 12));
-        Gender1.setBounds(260, 120, 100, 56);
-        add(Gender1);
 
-        JLabel adss = new JLabel("Address : ");
-        adss.setFont(new Font("Raleway", Font.BOLD, 12));
-        adss.setBounds(20, 170, 100, 56);
+        JLabel adss = new JLabel("Address :  "+w[5]);
+        adss.setFont(new Font("Raleway", Font.BOLD, 15));
+        adss.setBounds(20, 170, 500, 56);
         add(adss);
-        JLabel adss1 = new JLabel(w[5]);
-        adss1.setFont(new Font("Raleway", Font.BOLD, 12));
-        adss1.setBounds(90, 170, 300, 56);
-        add(adss1);
 
-        JLabel count = new JLabel("Country : ");
-        count.setFont(new Font("Raleway", Font.BOLD, 12));
-        count.setBounds(370, 170, 100, 56);
+        JLabel count = new JLabel("Country :  "+w[6]);
+        count.setFont(new Font("Raleway", Font.BOLD, 15));
+        count.setBounds(370, 170, 200, 56);
         add(count);
-        JLabel count1 = new JLabel(w[6]);
-        count1.setFont(new Font("Raleway", Font.BOLD, 12));
-        count1.setBounds(440, 170, 100, 56);
-        add(count1);
 
-        JLabel mail = new JLabel("Mail Address : ");
-        mail.setFont(new Font("Raleway", Font.BOLD, 12));
-        mail.setBounds(20, 220, 100, 56);
+        JLabel mail = new JLabel("Mail Address :  "+w[7]);
+        mail.setFont(new Font("Raleway", Font.BOLD, 15));
+        mail.setBounds(20, 220, 300, 56);
         add(mail);
-        JLabel mail1 = new JLabel(w[7]);
-        mail1.setFont(new Font("Raleway", Font.BOLD, 12));
-        mail1.setBounds(120, 220, 300, 56);
-        add(mail1);
 
-        JLabel phn = new JLabel("Phone Number : ");
-        phn.setFont(new Font("Raleway", Font.BOLD, 12));
-        phn.setBounds(20, 280, 100, 56);
+        JLabel phn = new JLabel("Phone Number :  "+w[8]);
+        phn.setFont(new Font("Raleway", Font.BOLD, 15));
+        phn.setBounds(20, 280, 300, 56);
         add(phn);
-        JLabel phn1 = new JLabel(w[8]);
-        phn1.setFont(new Font("Raleway", Font.BOLD, 12));
-        phn1.setBounds(120, 280, 100, 56);
-        add(phn1);
 
-        JLabel usr = new JLabel("Username : ");
-        usr.setFont(new Font("Raleway", Font.BOLD, 12));
-        usr.setBounds(20, 320, 100, 56);
+        JLabel usr = new JLabel("Username :  "+w[9]);
+        usr.setFont(new Font("Raleway", Font.BOLD, 15));
+        usr.setBounds(20, 320, 300, 56);
         add(usr);
-        JLabel usr1 = new JLabel(w[9]);
-        usr1.setFont(new Font("Raleway", Font.BOLD, 12));
-        usr1.setBounds(120, 320, 100, 56);
-        add(usr1);
 
         bback = new JButton("Back");
         bback.setFont(new Font("Raleway", Font.BOLD, 12));
