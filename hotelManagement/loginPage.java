84d1f8c8cb9edd60bd1670aa37e9c8fef9b86f3c
arvind-26 <ak5943436@gmail.com>
2024-01-31 23:25:26 +0530

    login page advance

--- hotelManagement/loginPage.java
+++ hotelManagement/loginPage.java
@@ -1,59 +1,87 @@
 package hotelManagement;
 
 import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 
-class loginPage extends JFrame implements ActionListener {
-  JLabel ltitle, luser, lpass, lor;
+class loginPage extends JFrame implements ActionListener  {
+  JLabel ltitle, luser, lpass, lor, ltitle2;
   JTextField tuser;
   JPasswordField ppass;
   JButton blogin, bsignin;
+  JPanel plogin,psign;
 
   public loginPage() {
-    setSize(450, 400);
-    setLocation(440, 170);
+    setSize(850, 478);
+    setLocationRelativeTo(null);
     setLayout(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
+    getContentPane().setBackground(Color.WHITE);
 
-    ltitle = new JLabel("Login");
-    ltitle.setFont(new Font("Ralway", Font.BOLD, 30));
-    ltitle.setBounds(10, 0, 80, 40);
-    add(ltitle);
+    psign = new JPanel();
+    psign.setBounds(425, 0, 425, 478);
+    psign.setBackground(new Color(225,95,31));
+    psign.setLayout(null);
+    add(psign);
 
-    luser = new JLabel("Username:");
-    luser.setFont(new Font("Ralway", Font.BOLD, 15));
-    luser.setBounds(67, 100, 120, 20);
-    add(luser);
+    plogin = new JPanel();
+    plogin.setBounds(0, 0, 425, 478);    
+    plogin.setBackground(Color.WHITE);
+    plogin.setLayout(null);
+    add(plogin);
+
+    ltitle = new JLabel("LOGIN");
+    ltitle.setFont(new Font("Times", Font.BOLD, 38));
+    ltitle.setForeground(new Color(225,95,31));
+    ltitle.setBounds(150, 40, 140, 40);
+    plogin.add(ltitle);
+
+    luser = new JLabel("Username");
+    luser.setFont(new Font("Ralway", Font.BOLD, 13));
+    luser.setBounds(40, 150, 120, 20);
+    plogin.add(luser);
 
     tuser = new JTextField();
-    tuser.setBounds(180, 100, 120, 20);
-    add(tuser);
+    tuser.setBounds(40, 175, 350, 30);
+    tuser.setFont(new Font("Ralway", Font.PLAIN, 17));
+    plogin.add(tuser);
 
-    lpass = new JLabel("Password:");
-    lpass.setFont(new Font("Ralway", Font.BOLD, 15));
-    lpass.setBounds(67, 150, 120, 20);
-    add(lpass);
+    lpass = new JLabel("Password");
+    lpass.setFont(new Font("Ralway", Font.BOLD, 13));
+    lpass.setBounds(40, 240, 120, 20);
+    plogin.add(lpass);
 
     ppass = new JPasswordField();
-    ppass.setBounds(180, 150, 120, 20);
-    add(ppass);
+    ppass.setBounds(40, 265, 350, 30);
+    ppass.setFont(new Font("Ralway", Font.PLAIN, 17));
+    plogin.add(ppass);
 
     blogin = new JButton("Login");
-    blogin.setBounds(180, 190, 100, 30);
-    add(blogin);
+    blogin.setBounds(156, 343, 110, 35);
+    blogin.setBackground(new Color(225,95,31));
+    blogin.setForeground(Color.WHITE);
+    blogin.setFont(new FontUIResource("Times", Font.PLAIN, 15));
+    blogin.setBorderPainted(false);
+    plogin.add(blogin);
     blogin.addActionListener(this);
 
-    lor = new JLabel("-------OR-------");
-    lor.setFont(new Font("Ralway", Font.ITALIC, 15));
-    lor.setBounds(180, 235, 120, 20);
-    add(lor);
+    ltitle2 = new JLabel("CREATE ACCOUNT");
+    ltitle2.setFont(new Font("Times", Font.BOLD, 38));
+    ltitle2.setForeground(Color.WHITE);
+    ltitle2.setBounds(20, 40, 380, 40);
+    psign.add(ltitle2);
 
     bsignin = new JButton("Sign in");
-    bsignin.setBounds(180, 280, 100, 30);
-    add(bsignin);
+    bsignin.setBounds(156, 343, 110, 35);
+    bsignin.setBackground(Color.white);
+    bsignin.setForeground(new Color(225,95,31));
+    bsignin.setFont(new FontUIResource("Times", Font.PLAIN, 15));
+    bsignin.setBorderPainted(false);
+    psign.add(bsignin);
     bsignin.addActionListener(this);
     setResizable(false);
     setVisible(true);
